83248acdf1aff686aab2f96d47266c2957bf373c
lokesh <karrilokesh108@gmail.com>
2025-03-05 21:35:49 +0530

    updated

--- clg_web/settings.py
+++ clg_web/settings.py
@@ -115,9 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Configure static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
 
 # Directory where collected static files will be stored
 STATIC_ROOT = BASE_DIR / "staticfiles"  # Add this line
