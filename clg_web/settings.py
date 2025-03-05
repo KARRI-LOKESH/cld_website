8af815c5898f8d2c63bbb100093ed2f126e22e42
lokesh <karrilokesh108@gmail.com>
2025-03-05 23:00:54 +0530

    lkiuyftfg

--- clg_web/settings.py
+++ clg_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c@$ikp__+%ryt&4i0a8013i52kbgyc6l1dvz_^@pf-^!p2*$f4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,16 +114,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Configure static files
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
-
-# Directory where collected static files will be stored
-STATIC_ROOT = BASE_DIR / "staticfiles"  # Add this line
 
 # Media files (if you have any file uploads)
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
