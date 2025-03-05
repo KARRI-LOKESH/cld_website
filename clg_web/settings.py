0c966a5102571ce30fd420f9ef46afac61ea646b
lokesh <karrilokesh108@gmail.com>
2025-03-05 21:05:08 +0530

    gfughj

--- clg_web/settings.py
+++ clg_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@$ikp__+%ryt&4i0a8013i52kbgyc6l1dvz_^@pf-^!p2*$f4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
